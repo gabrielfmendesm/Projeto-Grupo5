bbad5e5ca64bcf103039a6268e50fcd188fe6395
gabrielfmendesm <gabrielfmm@al.insper.edu.br>
2024-11-04 10:23:54 -0300

    Reset

--- src/main/java/br/Insper/Grupo_5_Projeto/RecomendacoesManuais/repository/RecomendacoesManuaisRepository.java
+++
@@ -1,7 +0,0 @@
-package br.Insper.Grupo_5_Projeto.RecomendacoesManuais.repository;
-
-import br.Insper.Grupo_5_Projeto.RecomendacoesManuais.model.RecomendacaoManual;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RecomendacoesManuaisRepository extends MongoRepository<RecomendacaoManual, String> {
-}
